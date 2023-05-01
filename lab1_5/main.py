401fc2f393564b44905b5e01fd19a3190a907b07
a-milek <44343010+a-milek@users.noreply.github.com>
2023-05-01 17:41:54 +0200

    finished 1_5

---
+++ lab1_5/main.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+
+def fibonacci(num):
+    # obliczanie za pomocą wzoru Bineta
+    fib = ((1 / sqrt(5)) * ((1 + sqrt(5)) / 2) ** num) - ((1 / sqrt(5)) * ((1 - sqrt(5)) / 2) ** num)
+    return fib
+
+
+if __name__ == '__main__':
+    print(int(fibonacci(16)))
