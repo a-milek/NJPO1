53eb3cefb0b107dea7e833dfdaf1a615c1c9fcf6
a-milek <44343010+a-milek@users.noreply.github.com>
2023-05-01 21:16:12 +0200

    finished 1_3

--- lab1_3/main.py
+++ lab1_3/main.py
@@ -1,8 +1,15 @@
-#Zmodyfikuj kod związany z losowaniem liczb z przedziału od 1 do 10,
+# Zmodyfikuj kod związany z losowaniem liczb z przedziału od 1 do 10,
 # tak aby obliczał przybliżoną wartość oczekiwaną obliczoną jako średnią (z prób).
 # Uśrednienie ma nastąpić 1m razy (milion razy).
+import random
+
+def wartosc_oczekiwana():
+    n = 1000000
+    suma = 0
+    for i in range(n):
+        suma = suma + random.randint(1, 10)
+    return suma / n
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    print(wartosc_oczekiwana())
