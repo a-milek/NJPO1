3272109c1d0cfcb367367e7dcc2203bc737d357b
a-milek <44343010+a-milek@users.noreply.github.com>
2023-05-04 21:13:12 +0200

    Finished 2_3

---
+++ lab2_3/main.py
@@ -0,0 +1,41 @@
+# Zadanie 3
+# Stwórz kolekcję książek zawierającą takie pola jak
+# `tytuł`,`gatunek`,`autor`,`isbn`.
+# Napisz trzy funkcje,
+# (i) zapisującą kolekcję,
+# (ii) odczytującą kolekcję,
+# (iii) obliczająca statystykę wg. gatunku i autora.
+import pickle
+from collections import namedtuple
+
+
+def save_collection(collection):
+    with open("books.pkl", "wb") as file:
+        pickle.dump(collection, file)
+
+
+def import_collection(file_name):
+    with open(file_name, 'rb') as file:
+        collection = pickle.load(file)
+    return collection
+
+
+def statistics(value, collection):
+    count = 0
+    for book in collection:
+        if value in book:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    Book = namedtuple('Book', ['title', 'genre', 'author', 'isbn'])
+
+    save_collection([Book('Miecz Przeznaczenia', 'Fantasy', 'Sapkowski', 282),
+                     Book('Hobbit', 'Fantasy', 'Tolkien', 619),
+                     Book('Krew Elfow', 'Fantasy', 'Sapkowski', 231),
+                     Book('Wieza Jaksolki', 'Fantasy', 'Sapkowski', 32),
+                     Book('Brak Tchu', 'Novel', 'Orwell', 1232)
+                     ])
+    col = import_collection('books.pkl')
+    print(statistics('Sapkowski', col))
