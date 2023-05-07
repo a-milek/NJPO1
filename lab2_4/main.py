a864b9b770adb7a589bc17b4aa0e70d15c4fc2a8
a-milek <44343010+a-milek@users.noreply.github.com>
2023-05-07 16:42:28 +0200

    gave up

---
+++ lab2_4/main.py
@@ -0,0 +1,7 @@
+# Zadanie 4
+# Za pomocą `namedtuple`i innych poznanych kolekcji zaimplementuj algorytm tworzący drzewo binarne.
+
+from collections import namedtuple
+
+# Define the node structure
+Node = namedtuple('Node', ['value', 'left', 'right'])
