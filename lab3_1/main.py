26be8788a0a0d71ceea89120e913dfb3cec3f9b3
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-28 17:26:58 +0200

    Add `search_in_tree` and `compare_points` functions to implement searching the tree

--- lab3_1/main.py
+++ lab3_1/main.py
@@ -6,7 +6,7 @@ from operator import itemgetter
 
 
 def kdtree(list_of_points, depth=0):
-    if not points:
+    if not list_of_points:
         return None
 
     # Select axis based on depth so that axis cycles through all valid values
@@ -32,16 +32,43 @@ def kdtree(list_of_points, depth=0):
     return node
 
 
+def search_in_tree(node, point, depth):
+    if not node:
+        return False
+    if compare_points(node["location"], point):
+        return True
+
+    current_depth = depth % len(point)
+    if point[current_depth] < node["location"][current_depth]:
+        return search_in_tree(node["leftChild"], point, depth + 1)
+    else:
+        return search_in_tree(node["rightChild"], point, depth + 1)
+
+
+def search(node, point):
+    return search_in_tree(node, point, 0)
+
+
+def compare_points(point1, point2):
+    for i in range(len(point1)):
+        if point1[i] != point2[i]:
+            return False
+    return True
+
+
 # main
 if __name__ == '__main__':
     points = [(1, 0), (2, 40), (15, 16), (51, 12), (8, 1), (23, 5), (32, 12), (3, 9), (1, 5), (8, 3), (3, 20), (4, 12), (12, 5)]
     tree = kdtree(points)
     print(tree)
+    print(search(tree, (51, 12)))
 
     points = [(4, 3, 5), (2, 1, 3), (5, 4, 2), (9, 6, 7), (4, 7, 8), (8, 1, 9), (7, 2, 1), (6, 3, 4), (3, 9, 6)]
     tree = kdtree(points)
     print(tree)
+    print(search(tree, (3, 9, 6)))
 
     points = [(1, 3, 5, 8), (2, 1, 3, 4), (5, 4, 2, 1), (9, 6, 7, 2), (4, 7, 8, 3), (8, 1, 9, 4), (7, 2, 1, 5), (6, 3, 4, 6), (3, 9, 6, 7)]
     tree = kdtree(points)
     print(tree)
+    print(search(tree, (7, 2, 1, 5)))
